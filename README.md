cd217dfc15d53c69ea8b968f72105f28649f016c
Dominik Krautstengel <dominik.placr@applifting.cz>
2023-04-05 15:50:19 +0200

    test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GherkinCypressTranslator
+
+---
+
+A tool for translating BDD Gherkin syntax test cases into ready-made code for implementing test-cases.
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./app.js";
-import "./styles.css";
-import "../node_modules/@picocss/pico/css/pico.css";
-import "./assets/images/cucumber.png";
-import "./assets/images/cypress.svg";
-
-// const cucumberLogo = require("./assets/images/cucumber-logo.png");
-// const cypressLogo = require("./assets/images/cypress-logo.png");
